e7baed73655154ba0f5c93ba47cd9b7e4ed6eb9e
moi <user@user.com>
2025-07-29 10:54:45 +0200

    name prompt

---
+++ todolist/src/main/java/com/test/Main.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("what is your name ?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
